510c00b45cf626b6e1c7d27c9820ac203d16cc48
diegojojoayandun <3922@holbertonschool.com>
2022-02-13 14:28:40 -0500

    Add 0 and 2 Task

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * selection_sort  - sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ *
+ * @array: The array to be order
+ * @size: Number of elements in @array
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, tmp, menor;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		menor =  i;
+
+		for (j = i + 1; j < size; j++)
+		{
+
+			if (array[j] < array[menor])
+				menor = j;
+
+		}
+		tmp = array[i];
+		array[i] = array[menor];
+		array[menor] = tmp;
+
+		if (menor != i)
+			print_array(array, size);
+	}
+
+}
